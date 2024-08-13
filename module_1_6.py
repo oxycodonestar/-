a5b3f6aae4a3fba75a654e0a864f168aedf802ef
stepka_pastukhov <rstepan368@gmail.com>
2024-08-13 21:39:26 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,12 @@
+my_dict = {'anton': 2002, 'danya': 1999, 'aleks': 2005,}
+print(my_dict['danya'])
+my_dict['dasha'] = 1989
+print(my_dict)
+my_dict.update({'login': 1212, 'password': 4444})
+del my_dict['anton']
+print(my_dict)
+
+my_set = {1,2,3,1,2,3, 'hello', ('Привет, я люблю Python')}
+print(my_set)
+print(my_set.remove('hello'))
+print(my_set)
